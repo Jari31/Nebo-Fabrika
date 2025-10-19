d74d6231eac9dd0bdf664dd3310665502e56f95f
Jari31 <159452778+Jari31@users.noreply.github.com>
2025-10-19 14:15:32 +0600

    Init C++
    Added basic C++ files to get started. (.gdextension is pending)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Undecided_Game_Proj
-A community porject led by Jari.
+A community project led by Jari.
+
+# Supported Platforms
+Windows, as of now.
---
+++ Undecided_Game_Proj/src/Test/test.h
@@ -0,0 +1,15 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <godot_cpp/classes/mesh_instance3d.hpp>
+
+namespace godot {
+
+    class GDExample : public MeshInstance3D {
+        GDCLASS(GDExample, MeshInstance3D)
+
+        
+    }
+}
+
+#endif
